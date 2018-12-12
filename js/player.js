737b3200b226f49921d812beab92520d20276b50
Andreas Heimann <andreas.heimann@hotmail.de>
2018-12-12 18:46:23 +0100

    refactored ResourceGen

--- js/player.js
+++ js/player.js
@@ -1,6 +1,19 @@
 class Player {
   constructor() {
-    this.material = 100;
-    this.manpower = 100;
+    this.typeDescriptions = ["material", "manpower"];
+
+    this.material = 0;
+    this.manpower = 0;
+  }
+
+  addResourceValue(resource) {
+    if (resource.typeDesc == this.typeDescriptions[0]) {
+      this.material += resource.clickValue;
+    } else if (resource.typeDesc == this.typeDescriptions[1]) {
+      this.manpower += resource.clickValue;
+    }
+
+    resource.miningChance = resource.baseMiningChance;
+    resource.currCharges--;
   }
 }
